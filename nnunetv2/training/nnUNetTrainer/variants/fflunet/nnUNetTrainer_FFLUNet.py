6855ee773dbf39176928b1444ba9623212bf375d
Sandip Dutta <sandtta@amazon.com>
2024-09-17 21:38:53 +0530

    Added Deep Supervision models

--- nnunetv2/training/nnUNetTrainer/variants/fflunet/nnUNetTrainer_FFLUNet.py
+++ nnunetv2/training/nnUNetTrainer/variants/fflunet/nnUNetTrainer_FFLUNet.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn.functional as nnfx
 from torch import nn
 
+from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
 from nnunetv2.training.nnUNetTrainer.variants.fflunet.blocks import (
     DepthWiseSeparableConvolution,
     DownSample,
@@ -20,11 +21,12 @@ from nnunetv2.utilities.plans_handling.plans_handler import (
 
 
 class FFLUNet(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, deep_supervision=False):
         super().__init__()
 
         self.in_channels = in_channels
         self.out_channels = out_channels
+        self.deep_supervision = deep_supervision
 
         self.input = Input(in_channels, 32)
 
@@ -57,7 +59,13 @@ class FFLUNet(nn.Module):
         self.w2 = nn.Parameter(torch.rand(2), requires_grad=True)
         self.w1 = nn.Parameter(torch.rand(2), requires_grad=True)
 
-        self.output = Output(32, out_channels)
+        # Output
+        if deep_supervision:
+            self.o3 = Output(128, out_channels)
+            self.o2 = Output(64, out_channels)
+            self.o1 = Output(32, out_channels)
+        else:
+            self.o = Output(32, out_channels)
 
     def forward(self, x):
         x = self.input(x)
@@ -80,12 +88,14 @@ class FFLUNet(nn.Module):
         w1 = nnfx.softmax(self.w1, dim=0)
 
         x = w4[0] * self.u4(x) + w4[1] * self.s4(x4)
-        x = w3[0] * self.u3(x) + w3[1] * self.s3(x3)
-        x = w2[0] * self.u2(x) + w2[1] * self.s2(x2)
-        x = w1[0] * self.u1(x) + w1[1] * self.s1(x1)
+        x = x3 = w3[0] * self.u3(x) + w3[1] * self.s3(x3)
+        x = x2 = w2[0] * self.u2(x) + w2[1] * self.s2(x2)
+        x = x1 = w1[0] * self.u1(x) + w1[1] * self.s1(x1)
 
-        x = self.output(x)
-        return x
+        if self.deep_supervision:
+            return [self.o1(x1), self.o2(x2), self.o3(x3)]
+        else:
+            return self.o(x)
 
 
 class nnUNetTrainer_FFLUNet(nnUNetTrainerNoDeepSupervision):
@@ -102,4 +112,23 @@ class nnUNetTrainer_FFLUNet(nnUNetTrainerNoDeepSupervision):
         return FFLUNet(
             in_channels=num_input_channels,
             out_channels=num_op_channels,
+            deep_supervision=False,
+        )
+
+
+class nnUNetTrainer_FFLUNetDS(nnUNetTrainer):
+    @staticmethod
+    def build_network_architecture(
+        plans_manager: PlansManager,
+        dataset_json,
+        configuration_manager: ConfigurationManager,
+        num_input_channels,
+        enable_deep_supervision: bool = False,
+    ) -> nn.Module:
+        label_manager = plans_manager.get_label_manager(dataset_json)
+        num_op_channels = label_manager.num_segmentation_heads
+        return FFLUNet(
+            in_channels=num_input_channels,
+            out_channels=num_op_channels,
+            deep_supervision=True,
         )
--- nnunetv2/training/nnUNetTrainer/variants/fflunet/nnUNetTrainer_FFLUNetAttentionDynamicShift.py
+++ nnunetv2/training/nnUNetTrainer/variants/fflunet/nnUNetTrainer_FFLUNetAttentionDynamicShift.py
@@ -3,6 +3,7 @@ import torch.nn.functional as nnx
 from monai.networks.blocks import ChannelSELayer
 from torch import nn
 
+from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
 from nnunetv2.training.nnUNetTrainer.variants.fflunet.blocks import (
     DownSample,
     Input,
@@ -20,11 +21,12 @@ from nnunetv2.utilities.plans_handling.plans_handler import (
 
 
 class FFLUNetAttentionDynamicShift(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, deep_supervision=False):
         super().__init__()
 
         self.in_channels = in_channels
         self.out_channels = out_channels
+        self.deep_supervision = deep_supervision
 
         self.input = Input(in_channels, 32)
 
@@ -58,7 +60,12 @@ class FFLUNetAttentionDynamicShift(nn.Module):
         self.w1 = nn.Parameter(torch.rand(2), requires_grad=True)
 
         # Output
-        self.output = OutputV2(32, out_channels)
+        if deep_supervision:
+            self.o3 = OutputV2(128, out_channels)
+            self.o2 = OutputV2(64, out_channels)
+            self.o1 = OutputV2(32, out_channels)
+        else:
+            self.o = OutputV2(32, out_channels)
 
     @staticmethod
     def get_dynamic_shifts(x: torch.Tensor):
@@ -85,13 +92,14 @@ class FFLUNetAttentionDynamicShift(nn.Module):
         w1 = nnx.softmax(self.w1, dim=0)
 
         x = w4[0] * self.u4(x) + w4[1] * self.s4(x4)
-        x = w3[0] * self.u3(x) + w3[1] * self.s3(x3)
-        x = w2[0] * self.u2(x) + w2[1] * self.s2(x2)
-        x = w1[0] * self.u1(x) + w1[1] * self.s1(x1)
+        x = x3 = w3[0] * self.u3(x) + w3[1] * self.s3(x3)
+        x = x2 = w2[0] * self.u2(x) + w2[1] * self.s2(x2)
+        x = x1 = w1[0] * self.u1(x) + w1[1] * self.s1(x1)
 
-        x = self.output(x)
-        return x
-
+        if self.deep_supervision:
+            return [self.o1(x1), self.o2(x2), self.o3(x3)]
+        else:
+            return self.o(x)
 
 class nnUNetTrainer_FFLUNetAttentionDynamicShift(nnUNetTrainerNoDeepSupervision):
     @staticmethod
@@ -107,4 +115,22 @@ class nnUNetTrainer_FFLUNetAttentionDynamicShift(nnUNetTrainerNoDeepSupervision)
         return FFLUNetAttentionDynamicShift(
             in_channels=num_input_channels,
             out_channels=num_op_channels,
+            deep_supervision=False,
+        )
+
+class nnUNetTrainer_FFLUNetAttentionDynamicShiftDS(nnUNetTrainer):
+    @staticmethod
+    def build_network_architecture(
+        plans_manager: PlansManager,
+        dataset_json,
+        configuration_manager: ConfigurationManager,
+        num_input_channels,
+        enable_deep_supervision: bool = True,
+    ) -> nn.Module:
+        label_manager = plans_manager.get_label_manager(dataset_json)
+        num_op_channels = label_manager.num_segmentation_heads
+        return FFLUNetAttentionDynamicShift(
+            in_channels=num_input_channels,
+            out_channels=num_op_channels,
+            deep_supervision=True
         )
